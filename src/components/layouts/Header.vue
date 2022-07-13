a29f39c3a571fd037d6fcae3855a1a51cd0bb9f0
ahmed7761 <sharif@namespaceit.com>
2022-07-13 19:28:39 +0600

    header component

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        {{title}}
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderComp",
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
